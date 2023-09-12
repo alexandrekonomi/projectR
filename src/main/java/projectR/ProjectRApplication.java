60b69b29616bfaed95988a4ecef5740aec53b469
alexandrekonomi <alexandrekonomi1234@gmail.com>
2023-09-12 15:51:58 -0300

    create project api

---
+++ src/main/java/projectR/ProjectRApplication.java
@@ -0,0 +1,13 @@
+package projectR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectR/ProjectRApplicationTests.java
@@ -0,0 +1,13 @@
+package projectR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
